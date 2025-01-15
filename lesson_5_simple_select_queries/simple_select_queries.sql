9c0c1f35301cca2d4d7e6a085039547fdd91eae9
Nihaochingiz <ilyaf1999@gmail.com>
2025-01-15 17:33:50 +0300

    Order all the cities in the United States

--- lesson_5_simple_select_queries/simple_select_queries.sql
+++ lesson_5_simple_select_queries/simple_select_queries.sql
@@ -1,9 +1,23 @@
 /*https://sqlbolt.com/lesson/select_queries_review*/
-
 /*
 List all the Canadian cities and their populations
-*/
+ */
+SELECT
+    city,
+    population
+FROM
+    north_american_cities
+WHERE
+    country = 'Canada';
 
-SELECT city,population FROM north_american_cities
-WHERE country = 'Canada'
-;
+/*
+Order all the cities in the United States by their latitude from north to south
+ */
+SELECT
+    city
+FROM
+    north_american_cities
+WHERE
+    country = 'United States'
+ORDER BY
+    latitude DESC;
